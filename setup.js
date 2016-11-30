73a81cc591e5d45a67ecf72a5802a423641c22f7
bo <bo.rhyne1@gmail.com>
2016-11-29 18:39:59 -0800

    updates

--- setup.js
+++ setup.js
@@ -100,14 +100,14 @@ function initCrossfilter() {
       .group(val1Grouping)
       .x(d3.scale.linear()
           .domain([0, 5000])
-          .rangeRound([0, 10 * 10])),
+          .rangeRound([0, 50 * 20])),
 
     barChart()
       .dimension(val2Dimension)
       .group(val2Grouping)
       .x(d3.scale.linear()
           .domain([0, 200000])
-          .rangeRound([0, 40 * 10]))
+          .rangeRound([0, 20 * 20]))
   ];
 
   // bind charts to dom
